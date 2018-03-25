4486a63dc6af0ecae35d8a091885402da74dd75e
User30000 <User30000@yandex.ru>
2018-03-26 00:26:18 +0700

    Tile extends aStarClass. Add boolean reachable for tile.

--- src/main/java/game/Map/Tile.java
+++ src/main/java/game/Map/Tile.java
@@ -1,24 +1,35 @@
 package game.Map;
 
-class Tile {
+import java.util.List;
+
+class Tile extends AStarNode {
     private int type;
     private int height;
     private int weight;
     private int visits;
 
+    public int posX;
+    public int posY;
+
+    public boolean reachable = true;
+
+    private Chunk parentChunk;
+
     Tile(int height) {
         this.height = height;
-        if(height < 10){
+        if (height < 10) {//water
             type = 3;
-        }
-        else if(height < 15){
+            weight = 1000;
+            reachable = false;
+        } else if (height < 15) {//sand
             type = 0;
-        }
-        else if (height < 90){
+            weight = 2;
+        } else if (height < 90) {//grass
             type = 2;
-        }
-        else {
+            weight = 1;
+        } else {//mountains
             type = 1;
+            weight = 3;
         }
         visits = 0;
     }
@@ -27,7 +38,33 @@ class Tile {
         return type;
     }
 
+    //int getWeight(){return weight;}
+
     float getHeight() {
         return height;
+    }
+
+    void setParentChunk(Chunk chunk) {
+        parentChunk = chunk;
+    }
+
+    void setPosition(int x, int y) {
+        posX = x;
+        posY = y;
+    }
+
+    @Override
+    public float getCost(AStarNode node) {
+        return weight;
+    }
+
+    @Override
+    public float getEstimatedCost(AStarNode node) {
+        return Math.abs(((Tile) node).height - this.height);
+    }
+
+    @Override
+    public List getNeighbors() {
+        return parentChunk.getNeighbors(posX, posY);
     }
 }
