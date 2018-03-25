485b956730c44ab2ee45ed8c3b9d9813dd5df63e
User30000 <User30000@yandex.ru>
2018-03-26 00:43:47 +0700

    Add properties class

---
+++ src/main/java/util/Prop.java
@@ -0,0 +1,42 @@
+package util;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
+
+public class Prop {
+    private static Prop instance;
+    private Properties properties;
+
+    private Prop() {
+        properties = new Properties();
+        try {
+            URL u = getClass().getResource("/config.properties");
+            InputStream input;
+            if (u != null) {
+                 input = getClass().getClassLoader().getResourceAsStream("config.properties");
+            }
+            else{
+                input = getClass().getClassLoader().getResourceAsStream("public.properties");
+            }
+
+            properties.load(input);
+
+            Enumeration<?> e = properties.propertyNames();
+            while (e.hasMoreElements()) {
+                String key = (String) e.nextElement();
+                String value = properties.getProperty(key);
+            }
+        } catch (Exception e) {
+            System.out.println("Не обнаружен файл config.properties");
+        }
+    }
+
+    public static String getProp(String key){
+        if (instance == null) {
+            instance = new Prop();
+        }
+        return instance.properties.getProperty(key);
+    }
+}
