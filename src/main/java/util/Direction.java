b7f83373a88d6795782451fcbc2724454bb75f3d
User30000 <User30000@yandex.ru>
2018-03-27 22:07:17 +0700

    Direction return random

--- src/main/java/util/Direction.java
+++ src/main/java/util/Direction.java
@@ -1,8 +1,16 @@
 package util;
 
+import java.util.Random;
+
 public enum Direction {
     UP,
     RIGHT,
     DOWN,
-    LEFT
+    LEFT;
+
+    public static Direction getRandom()  {
+        int size = values().length;
+        values()[1].name();
+        return values()[new Random().nextInt(size)];
+    }
 }
